ecd488c6d6222766ff0692dbef3b44c1c038cc26
Vijay Chandra <vijaychandra800@gmail.com>
2021-08-29 19:44:13 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,16 +32,16 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-          <meta name="title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta name="description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
+          <meta name="title" content="Vijay Chandra - Blogger & Android Dev" />
+          <meta name="description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta property="og:description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
-          <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
+          <meta property="og:title" content="Vijay Chandra - Blogger & Android Dev" />
+          <meta property="og:description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
+          <meta property="og:image" content="https://atozhacks.in/banner.png" />
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:title" content="Vipul Jha - Android & Flutter Dev" />
-          <meta property="twitter:description" content="Hey there, This is Vipul Jha aka lordarcadius. I am an Android & Flutter developer who also works on ROMs, Kernels, & Scripts." />
-          <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" />
+          <meta property="twitter:title" content="Vijay Chandra - Blogger & Android Dev" />
+          <meta property="twitter:description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
+          <meta property="twitter:image" content="https://atozhacks.in/banner.png" />
         </Head>
         <body>
           <Main />
