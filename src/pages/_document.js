9a3b476c2015026674fadc2b24b84f92f7dee0ac
Vijay Chandra <vijaychandra800@gmail.com>
2021-08-30 10:11:46 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,14 +33,14 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="title" content="Vijay Chandra - Blogger & Android Dev" />
-          <meta name="description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
+          <meta name="description" content="Hey there, This is Vijay Chandra. I am a Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Vijay Chandra - Blogger & Android Dev" />
-          <meta property="og:description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
+          <meta property="og:description" content="Hey there, This is Vijay Chandra. I am a Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
           <meta property="og:image" content="https://atozhacks.in/banner.png" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:title" content="Vijay Chandra - Blogger & Android Dev" />
-          <meta property="twitter:description" content="Hey there, This is Vijay Chandra. I am an Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
+          <meta property="twitter:description" content="Hey there, This is Vijay Chandra. I am a Blogger, Freelancer, YouTuber, Android and Flutter Developer who also works on ROMs & Scripts." />
           <meta property="twitter:image" content="https://atozhacks.in/banner.png" />
         </Head>
         <body>
